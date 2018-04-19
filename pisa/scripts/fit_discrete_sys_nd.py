a23ee501f60454b0bb50e5c7cc2c2bd4f16c6353
Thomas Ehrhardt <tehrhard@uni-mainz.de>
2018-04-19 17:10:24 +0200

    minor formatting

--- pisa/scripts/fit_discrete_sys_nd.py
+++ pisa/scripts/fit_discrete_sys_nd.py
@@ -363,7 +363,8 @@ def fit_discrete_sys_distributions(
 
     for map_name, chan_norm_sys_maps in norm_sys_maps.items():
         logging.info( # pylint: disable=logging-not-lazy
-            'Fitting "%s" maps with initial guess %s.' % (map_name, p0[map_name])
+            'Fitting "%s" maps with initial guess %s.'
+            % (map_name, p0[map_name])
         )
 
         fit_results[map_name] = np.ones(shape_output)
@@ -444,9 +445,8 @@ def hyperplane(fit_cfg, set_param=None):
     if set_param:
         raise NotImplementedError()
 
-    nominal_mapset, sys_list, sys_param_points, sys_mapsets = make_discrete_sys_distributions(
-        fit_cfg=fit_cfg
-    )
+    nominal_mapset, sys_list, sys_param_points, sys_mapsets =\
+        make_discrete_sys_distributions(fit_cfg=fit_cfg)
 
     hyperplane_fits, errors, chi2s, binning = fit_discrete_sys_distributions(
         nominal_mapset=nominal_mapset,
@@ -454,7 +454,8 @@ def hyperplane(fit_cfg, set_param=None):
         sys_param_points=sys_param_points,
         sys_mapsets=sys_mapsets
     )
-    return nominal_mapset, sys_param_points, sys_mapsets, binning, hyperplane_fits, errors, chi2s
+    return (nominal_mapset, sys_param_points, sys_mapsets, binning,
+            hyperplane_fits, errors, chi2s)
 
 
 def save_hyperplane_fits(hyperplane_fits, chi2s, outdir, tag=None):
@@ -479,7 +480,8 @@ def save_hyperplane_fits(hyperplane_fits, chi2s, outdir, tag=None):
     np.save('%s/nd_sysfits_%s_raw_chi2s' % (outdir, tag), chi2s)
 
 
-def plot_hyperplane_fit_params(hyperplane_fits, names, binning, outdir=None, tag=None):
+def plot_hyperplane_fit_params(hyperplane_fits, names, binning, outdir=None,
+                               tag=None):
     """Plot 2D distributions of fit parameters.
 
     Parameters
@@ -567,15 +569,17 @@ def plot_chisquare_values(chi2s, outdir, fit=True, fit_loc_scale=True,
     logging.info('Histogramming %d chisquare values.' % len(chi2s)) # pylint: disable=logging-not-lazy
     if bins is None:
         bins = np.linspace(0.99*min(chi2s), 1.01*max(chi2s), 100)
-    fig = plt.figure(figsize=(10,8))
+    fig = plt.figure()
     n, bins, _ = plt.hist(
         chi2s, bins=bins, facecolor='firebrick', histtype='stepfilled',
         weights=np.ones_like(chi2s)/len(chi2s),
         label='hyperplane residuals (%d)' % len(chi2s)
     )
     if fit:
-        logging.info('Performing chisquare fit with "loc" and "scale" %s.'
-                     % ('floating' if fit_loc_scale else 'fixed'))
+        logging.info( # pylint: disable=logging-not-lazy
+            'Performing chisquare fit with "loc" and "scale" %s.'
+            % ('floating' if fit_loc_scale else 'fixed')
+        )
         if fit_loc_scale:
             # fit for d.o.f., location and scale of distribution of values
             popt = stats.chi2.fit(chi2s)
@@ -583,7 +587,7 @@ def plot_chisquare_values(chi2s, outdir, fit=True, fit_loc_scale=True,
         else:
             popt = stats.chi2.fit(chi2s, floc=0, fscale=1)
             fit_rv = stats.chi2(df=popt[0])
-        logging.info('Best fit parameters: %s' % list(popt))
+        logging.info('Best fit parameters: %s' % list(popt)) # pylint: disable=logging-not-lazy
         # plot the binwise integrated fit pdf
         fit_cdf = fit_rv.cdf(bins[1:]) - fit_rv.cdf(bins[:-1])
         centers = (bins[1:] + bins[:-1])/2.
