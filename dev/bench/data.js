6d0a31bb49b6bfadf46571182072e804e7318ac7
github-action-benchmark <github@users.noreply.github.com>
2026-06-06 12:43:44 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 565fd0947ae097a5c5fc5c25c52076afaee380e8

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1780749824289,
+  "repoUrl": "https://github.com/thehrh/pisa-1",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "thehrh@users.noreply.github.com",
+            "name": "T Ehrhardt",
+            "username": "thehrh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "565fd0947ae097a5c5fc5c25c52076afaee380e8",
+          "message": "benchmark workflow: change Pages branch and benchmark data path again",
+          "timestamp": "2026-06-06T14:40:41+02:00",
+          "tree_id": "77d54217fe3cfb8bc3fd0bd4bb540cb472afe698",
+          "url": "https://github.com/thehrh/pisa-1/commit/565fd0947ae097a5c5fc5c25c52076afaee380e8"
+        },
+        "date": 1780749823537,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "IceCube_3y_neutrinos_daemon (parallel, nthreads=4)",
+            "value": 0.4848251634714555,
+            "range": "0.20575904846191406",
+            "unit": "s",
+            "extra": "target=parallel, nthreads=4"
+          },
+          {
+            "name": "IceCube_3y_neutrinos (parallel, nthreads=4)",
+            "value": 0.4102173386787882,
+            "range": "0.17945408821105957",
+            "unit": "s",
+            "extra": "target=parallel, nthreads=4"
+          },
+          {
+            "name": "IceCube_3y_muons (parallel, nthreads=4)",
+            "value": 0.0005620158448511241,
+            "range": "0.005359172821044922",
+            "unit": "s",
+            "extra": "target=parallel, nthreads=4"
+          }
+        ]
+      }
+    ]
+  }
+}
