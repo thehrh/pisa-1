d7b11d09052cf84e3db69a631f6b30c1ef7f9fc3
github-action-benchmark <github@users.noreply.github.com>
2026-06-06 12:44:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 565fd0947ae097a5c5fc5c25c52076afaee380e8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780749824289,
+  "lastUpdate": 1780749867891,
   "repoUrl": "https://github.com/thehrh/pisa-1",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "range": "0.005359172821044922",
             "unit": "s",
             "extra": "target=parallel, nthreads=4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thehrh@users.noreply.github.com",
+            "name": "T Ehrhardt",
+            "username": "thehrh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "565fd0947ae097a5c5fc5c25c52076afaee380e8",
+          "message": "benchmark workflow: change Pages branch and benchmark data path again",
+          "timestamp": "2026-06-06T14:40:41+02:00",
+          "tree_id": "77d54217fe3cfb8bc3fd0bd4bb540cb472afe698",
+          "url": "https://github.com/thehrh/pisa-1/commit/565fd0947ae097a5c5fc5c25c52076afaee380e8"
+        },
+        "date": 1780749867385,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "IceCube_3y_neutrinos_daemon (cpu, nthreads=1)",
+            "value": 0.8172837228191142,
+            "range": "0.27794504165649414",
+            "unit": "s",
+            "extra": "target=cpu, nthreads=1"
+          },
+          {
+            "name": "IceCube_3y_neutrinos (cpu, nthreads=1)",
+            "value": 0.9007921608126893,
+            "range": "0.21947169303894043",
+            "unit": "s",
+            "extra": "target=cpu, nthreads=1"
+          },
+          {
+            "name": "IceCube_3y_muons (cpu, nthreads=1)",
+            "value": 0.0004404758920474928,
+            "range": "0.0005295276641845703",
+            "unit": "s",
+            "extra": "target=cpu, nthreads=1"
           }
         ]
       }
